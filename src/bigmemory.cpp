f58542e0b804f5aea788caf7f5cc62eeeb8cab0c
Charles Determan <cdetermanjr@hotmail.com>
2015-04-30 12:45:52 -0500

    try to explicitly convert from long long int to int

--- src/bigmemory.cpp
+++ src/bigmemory.cpp
@@ -1413,7 +1413,7 @@ SEXP CGetType(SEXP bigMatAddr)
 {
   Rcpp::XPtr<BigMatrix> pMat(bigMatAddr);
   int ret = pMat->matrix_type();
-  return Rcpp::wrap(ret);
+  return Rcpp::wrap((int)ret);
 }
 
 // not currently used?!?!?!
